bbf6ff61485d909ffd3ddffd2c5c0674188627cb
MoopleMaybe <byrdcarter09@gmail.com>
2025-09-28 23:43:36 -0500

    Update index.js
    ok

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,13 @@
-<iframe src="https://www.retrogames.cc/embed/45032-sonic-the-fighters-japan.html" width="600" height="450" frameborder="no" allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true" scrolling="no"></iframe>
+<!DOCTYPE html>
+
+<html>
+<head>
+    <title>Sonic the Fighers!!!</title>
+</head>
+<body>
+
+    <iframe style="border:0" src=<iframe src="https://www.retrogames.cc/embed/45032-sonic-the-fighters-japan.html" width="600" height="450" frameborder="no" allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true" scrolling="no"></iframe>
+
+</body>
+</html>
+
